743ede5e066f599ba72a1c634453940b723d9924
Joan Gabriel Salas Silva <82409450+DarkRose73@users.noreply.github.com>
2023-06-19 11:14:21 -0400

    creacion modulo y pagina principal

---
+++ src/app/graficos/graficos.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraficosComponent } from './pages/graficos/graficos.component';
+
+@NgModule({
+  declarations: [GraficosComponent],
+  imports: [CommonModule],
+  exports: [GraficosComponent],
+})
+export class GraficosModule {}
