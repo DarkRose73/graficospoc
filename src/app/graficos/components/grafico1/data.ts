8c0161a870b59e2a7aa0009a44e9739205a41382
Joan Gabriel Salas Silva <82409450+DarkRose73@users.noreply.github.com>
2023-06-19 12:40:04 -0400

    grafico1 wip

---
+++ src/app/graficos/components/grafico1/data.ts
@@ -0,0 +1,19 @@
+// TODO: MOVER A SERVICE
+const dataset = [
+  { data: [653, 250, 1000, 100, 56, 55, 40], label: 'ROJO', stack: 'A' },
+  { data: [282, 250, 250, 20, 86, 27, 90], label: 'AZUL', stack: 'A' },
+];
+
+// Calcular el total para cada punto del dataset
+const totals: number[] = dataset[0].data.map((_, dataIndex) =>
+  dataset.reduce((sum, series) => sum + series.data[dataIndex], 0)
+);
+
+// Calcular los datos normalizados
+export const NORMALIZED_DATA = dataset.map((series) => ({
+  data: series.data.map(
+    (value, dataIndex) => (value / totals[dataIndex]) * 100
+  ),
+  label: series.label,
+  stack: series.stack,
+}));
--- src/app/graficos/components/grafico1/grafico1.component.ts
+++ src/app/graficos/components/grafico1/grafico1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
+import { NORMALIZED_DATA } from './data';
 
 @Component({
   selector: 'graficos-grafico1',
@@ -11,15 +12,33 @@ export class Grafico1Component {
   public barChartPlugins = [];
 
   public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', stack: 'A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', stack: 'A' },
-    ],
+    labels: ['', '', '', '', '', '', ''],
+    datasets: NORMALIZED_DATA,
   };
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
+
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: 'Producto',
+        },
+      },
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: 'Impuesto',
+        },
+        ticks: {
+          callback: (value) => {
+            return value + '%';
+          },
+        },
+      },
+    },
   };
 
   constructor() {}
