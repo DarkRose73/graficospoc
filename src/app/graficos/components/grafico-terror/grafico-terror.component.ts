d32f0146a1cef643ad5b955885db6c30d263fc8c
Joan Gabriel Salas Silva <82409450+DarkRose73@users.noreply.github.com>
2023-06-23 09:41:28 -0400

    cambios varios

--- src/app/graficos/components/grafico-terror/grafico-terror.component.ts
+++ src/app/graficos/components/grafico-terror/grafico-terror.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ChartDataset, ChartConfiguration } from 'chart.js';
 import { GraficosService } from '../../services/graficos-service.service';
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'graficos-grafico-terror',
@@ -11,24 +12,32 @@ export class GraficoTerrorComponent {
   constructor(private graficosService: GraficosService) {}
 
   public barChartLegend = true;
-  public barChartPlugins = [];
+  public barChartPlugins = [DataLabelsPlugin];
+  // TODO: Definir colores de labels
   private dataset: ChartDataset<'bar', number[]>[] = [
-    { data: [0, -24, 9, 0, -13, -6, 0, -7], label: 'IE var', stack: 'A' },
+    {
+      data: [0, -24, 9, 0, -13, -6, 0, -7],
+      label: 'IE var',
+      stack: 'A',
+      backgroundColor: '#ffbc01',
+    },
     {
       data: [371, 372, 372, 372, 372, 375, 375, 375],
       label: 'IE fijo',
       stack: 'A',
-      pointStyle: 'circle',
+      backgroundColor: '#a4a4a4',
     },
     {
       data: [125, 129, 124, 122, 124, 122, 123, 124],
       label: 'IVA',
       stack: 'A',
+      backgroundColor: '#fbfbfb',
     },
     {
       data: [660, 679, 651, 640, 651, 643, 649, 655],
       label: 'Costo ENAP',
       stack: 'A',
+      backgroundColor: '#cbcbcb',
     },
   ];
 
@@ -48,7 +57,44 @@ export class GraficoTerrorComponent {
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: true,
+    scales: {
+      y: {
+        display: false,
+      },
+      x: {
+        display: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
     plugins: {
+      datalabels: {
+        anchor: 'center',
+        font: {
+          weight: 'bold',
+        },
+        // formatter: function (value, context) {
+        //   // Obtener los valores o etiquetas correspondientes
+        //   var dataset = context.dataset;
+        //   var index = context.dataIndex;
+        //   var values = dataset.data;
+
+        //   // Crear una cadena con las etiquetas separadas por una coma
+        //   var labels = ['Label 1', 'Label 2', 'Label 3']; // Ejemplo de etiquetas
+        //   var labelString = labels[index];
+
+        //   return labelString;
+        // },
+        // backgroundColor(context) {
+        //   switch (context.datasetIndex) {
+        //     case 0:
+        //       return '#ffc400';
+        //     default:
+        //       return '';
+        //   }
+        // },
+      },
       legend: {
         align: 'center',
         position: 'right',
